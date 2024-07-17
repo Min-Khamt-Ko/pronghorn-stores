583ac203225bb31b484a1407e6afad49080e479f
Min-Khamt-Ko <minkhamtko99@gmail.com>
2024-07-18 05:41:22 +0630

    pronghorn website

---
+++ View/index.view.php
@@ -0,0 +1,7 @@
+<?php require "partials/head.php" ?>
+
+<section class="w-full bg-violet-400 text-center text-violet-50 py-3">
+    <p>Purchase at least 30,000MMK get 20% discount on all items. Discount end in next 3:35hrs. Grab the chance!!!</p>
+</section>
+
+<?php require "partials/footer.php" ?>
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . "Core/function.php";
+
+require base_path('view/index.view.php');
+
