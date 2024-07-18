7ee8906b0229e47432a17009198528df89aa9ba0
Min-Khamt-Ko <minkhamtko99@gmail.com>
2024-07-18 22:08:59 +0630

    latest file

--- View/index.view.php
+++ View/index.view.php
@@ -1,7 +1,5 @@
 <?php require "partials/head.php" ?>
+<?php require "partials/banner.php" ?>
 
-<section class="w-full bg-violet-400 text-center text-violet-50 py-3">
-    <p>Purchase at least 30,000MMK get 20% discount on all items. Discount end in next 3:35hrs. Grab the chance!!!</p>
-</section>
 
 <?php require "partials/footer.php" ?>
