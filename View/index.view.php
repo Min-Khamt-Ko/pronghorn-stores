fbd6c626b0b780283f6a751f518f7923e66db87f
Min-Khamt-Ko <minkhamtko99@gmail.com>
2024-07-19 00:24:27 +0630

    done nav and hero section

--- View/index.view.php
+++ View/index.view.php
@@ -1,5 +1,46 @@
 <?php require "partials/head.php" ?>
 <?php require "partials/banner.php" ?>
 
+<?php require "partials/nav.php" ?>
+
+    <main>
+        <div class="text-center mt-40">
+            <h1 class="text-5xl font-bold">Mid-Season Sale</h1>
+            <button class="mt-10 bg-gray-400 py-3 px-8 rounded-xl text-white">
+                <a href="#">Shop Collection</a>
+            </button>
+        </div>
+        <!--    img section-->
+        <section class="img-section px-8 mx-auto">
+            <div class="mt-20">
+                <div class="grid grid-cols-12">
+                    <div class="col-span-4 col-start-2 col-end-4 relative">
+                        <img src="https://images.unsplash.com/photo-1578587018452-892bacefd3f2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                             alt="Image 1" class="w-full h-96 object-cover rounded-xl">
+                        <div class="absolute bottom-5 left-5 text-orange-600">
+                        <p class="">Shop the collection</p>
+                        <p class="">Men's</p>
+                        </div>
+                    </div>
+                    <div class="col-span-4 col-start-6 col-end-8 relative">
+                        <img src="https://images.unsplash.com/photo-1578587018452-892bacefd3f2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                             alt="Image 1" class="w-full h-96 object-cover rounded-xl">
+                        <div class="absolute bottom-5 left-5 text-orange-600">
+                            <p class="">Shop the collection</p>
+                            <p class="">Men's</p>
+                        </div>
+                    </div>
+                    <div class="col-span-4 col-start-10 col-end-12 relative">
+                        <img src="https://images.unsplash.com/photo-1578587018452-892bacefd3f2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                             alt="Image 1" class="w-full h-96 object-cover rounded-xl">
+                        <div class="absolute bottom-5 left-5 text-orange-600">
+                            <p class="">Shop the collection</p>
+                            <p class="">Men's</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
 <?php require "partials/footer.php" ?>
---
+++ View/partials/nav.php
@@ -0,0 +1,18 @@
+<nav class="py-4 px-8">
+    <div class="flex items-center justify-between mr-4">
+    <div class="flex space-x-6 items-center">
+    <div class="logo">
+    <h1 class="text-3xl font-bold">Logo</h1>
+    </div>
+    <div class="text-md space-x-6">
+        <span><a href="#">Men</a></span>
+        <span><a href="#">Women</a></span>
+        <span><a href="#">Bags</a></span>
+        <span><a href="#">Stores</a></span>
+    </div>
+    </div>
+    <div class="flex gap-10 font-semibold">
+        <p>Search</p>
+        <a href="#">Login</a>
+    </div>
+</nav>
